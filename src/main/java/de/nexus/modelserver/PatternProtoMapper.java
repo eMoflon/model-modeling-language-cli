b5f61dfb58f00de02a918a94b48767108ade2fcb
JanikNex <18660622+JanikNex@users.noreply.github.com>
2024-02-14 18:33:07 +0100

    added serialized nodes to grpc pattern responses

--- src/main/java/de/nexus/modelserver/PatternProtoMapper.java
+++ src/main/java/de/nexus/modelserver/PatternProtoMapper.java
@@ -2,8 +2,10 @@ package de.nexus.modelserver;
 
 import de.nexus.modelserver.proto.ModelServerPatterns;
 
+import java.util.stream.Collectors;
+
 public class PatternProtoMapper {
     public static ModelServerPatterns.Pattern mapPattern(Pattern pattern) {
-        return ModelServerPatterns.Pattern.newBuilder().setName(pattern.getName()).setNumberOfMatches(pattern.getMatches().size()).build();
+        return ModelServerPatterns.Pattern.newBuilder().setName(pattern.getName()).setNumberOfMatches(pattern.getMatches().size()).addAllMatches(pattern.getMatches().stream().map(x -> ModelServerPatterns.Match.newBuilder().addAllNodes(x.getObjects().stream().map(Object::toString).collect(Collectors.toList())).build()).collect(Collectors.toList())).build();
     }
 }
