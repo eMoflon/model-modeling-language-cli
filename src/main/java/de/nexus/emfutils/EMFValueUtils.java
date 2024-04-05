9be56987f9826801971866083c56a4520d32f306
JanikNex <18660622+JanikNex@users.noreply.github.com>
2024-04-05 02:02:38 +0200

    added method to retrieve attribute value from SmartObject as ValueWrapper

--- src/main/java/de/nexus/emfutils/EMFValueUtils.java
+++ src/main/java/de/nexus/emfutils/EMFValueUtils.java
@@ -1,8 +1,11 @@
 package de.nexus.emfutils;
 
+import de.nexus.expr.ValueWrapper;
+import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EEnum;
 import org.eclipse.emf.ecore.EcorePackage;
+import org.emoflon.smartemf.runtime.SmartObject;
 
 public class EMFValueUtils {
     @SuppressWarnings("unchecked")
@@ -19,5 +22,13 @@ public class EMFValueUtils {
             case EcorePackage.EBOOLEAN -> (R) Boolean.valueOf(value.toString());
             default -> (R) value.toString();
         };
+    }
+
+    public static ValueWrapper<?> requestValue(SmartObject obj, String attrName) {
+        EAttribute attr = (EAttribute) obj.eClass().getEStructuralFeature(attrName);
+        if (attr == null) {
+            throw new RuntimeException(String.format("Trying to access %s.%s but it does not exist!", obj.eClass().getName(), attrName));
+        }
+        return ValueWrapper.fromEcoreValue(attr.getEAttributeType(), obj.eGet(attr));
     }
 }
