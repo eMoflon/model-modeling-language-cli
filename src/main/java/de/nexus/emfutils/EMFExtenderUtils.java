ad257c327c03bf4b6eda00051bf21f8ddf10b12e
JanikNex <18660622+JanikNex@users.noreply.github.com>
2024-04-25 20:48:54 +0200

    update extender settings for already extended Railway models

--- src/main/java/de/nexus/emfutils/EMFExtenderUtils.java
+++ src/main/java/de/nexus/emfutils/EMFExtenderUtils.java
@@ -8,8 +8,8 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class EMFExtenderUtils {
-    public static final String IDENTIFIER_CLASS_NAME = "MMLConstraintIdentifier";
-    public static final String IDENTIFIER_ATTRIBUTE_NAME = "nodeId";
+    public static final String IDENTIFIER_CLASS_NAME = "RailwayElement";
+    public static final String IDENTIFIER_ATTRIBUTE_NAME = "id";
 
     public static EClass getMMLIdentifierClass(EPackage ePackage) {
         List<EClassifier> classifiers = ePackage.getEClassifiers().stream().filter(x -> x instanceof EClass).filter(x -> ((EClass) x).isAbstract() && ((EClass) x).getESuperTypes().isEmpty() && x.getName().equals(IDENTIFIER_CLASS_NAME)).toList();
