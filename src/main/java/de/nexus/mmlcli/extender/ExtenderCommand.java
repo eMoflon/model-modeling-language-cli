a80de405a59a1cedca98d2ab7f57236a8aa8dc00
JanikNex <18660622+JanikNex@users.noreply.github.com>
2024-05-06 01:26:28 +0200

    update Extender command descriptions

--- src/main/java/de/nexus/mmlcli/extender/ExtenderCommand.java
+++ src/main/java/de/nexus/mmlcli/extender/ExtenderCommand.java
@@ -9,11 +9,11 @@ import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "extender", mixinStandardHelpOptions = true, version = "v1.0.0", description = "Extend a metamodel and model with a unique node identifier")
 public class ExtenderCommand implements Callable<Integer> {
-    @CommandLine.Parameters(index = "0")
+    @CommandLine.Parameters(index = "0", description = "path to the Ecore file containing the metamodel")
     File ecoreFile;
-    @CommandLine.Parameters(index = "1")
+    @CommandLine.Parameters(index = "1", description = "path to the XMI file containing the model")
     File modelFile;
-    @CommandLine.Option(names = {"-i", "--invert"}, arity = "0..1", defaultValue = "false", description = "remove identifiers if present")
+    @CommandLine.Option(names = {"-i", "--invert"}, paramLabel = "(true|false)", arity = "0..1", defaultValue = "false", description = "remove identifiers if present")
     private boolean inverted;
 
     @Override
